b38e4166f98f2aa9a6393c50f45af53e78c9a91e
vitalik <tarnavskij2002@gmail.com>
2022-10-22 14:41:39 +0300

    camera postition

--- main.py
+++ main.py
@@ -34,17 +34,17 @@ btnDoor1 = Button(root, text="", image=door, borderwidth=0, activebackground='#f
 btnDoor1.place(x=90, y=120)
 
 btnCam1 = Button(root, text="", image=cam, borderwidth=0, activebackground='#ffffff', background='#ffffff', relief=SUNKEN, command=lambda: switchCam('cam1', btnCam1))
-btnCam1.place(x=160, y=520)
+btnCam1.place(x=160, y=420)
 btnCam2 = Button(root, text="", image=cam, borderwidth=0, activebackground='#ffffff', background='#ffffff', relief=SUNKEN, command=lambda: switchCam('cam2', btnCam2))
-btnCam2.place(x=230, y=520)
+btnCam2.place(x=230, y=420)
 btnCam3 = Button(root, text="", image=cam, borderwidth=0, activebackground='#ffffff', background='#ffffff', relief=SUNKEN, command=lambda: switchCam('cam3', btnCam3))
-btnCam3.place(x=300, y=520)
+btnCam3.place(x=300, y=420)
 btnCam4 = Button(root, text="", image=cam, borderwidth=0, activebackground='#ffffff', background='#ffffff', relief=SUNKEN, command=lambda: switchCam('cam4', btnCam4))
-btnCam4.place(x=370, y=520)
+btnCam4.place(x=370, y=420)
 btnCam5 = Button(root, text="", image=cam, borderwidth=0, activebackground='#ffffff', background='#ffffff', relief=SUNKEN, command=lambda: switchCam('cam5', btnCam5))
-btnCam5.place(x=440, y=520)
+btnCam5.place(x=440, y=420)
 btnCam6 = Button(root, text="", image=cam, borderwidth=0, activebackground='#ffffff', background='#ffffff', relief=SUNKEN, command=lambda: switchCam('cam6', btnCam6))
-btnCam6.place(x=510, y=520)
+btnCam6.place(x=510, y=420)
 
 cams = [btnCam1, btnCam2, btnCam3, btnCam4, btnCam5, btnCam6]
 
