404289db48a1b058c33972c14671e68475740b04
Vitalik <tarnavskij2002@gmail.com>
2022-11-10 01:03:06 +0200

    cams

--- app.py
+++ app.py
@@ -440,9 +440,10 @@ class Window(Tk):
                 if self.status == False:
                     break
                 ret, image = cam.read()
-                cv2.namedWindow(image)
-                cv2.moveWindow(image, 1024, 0)
-                cv2.imshow('Imagetest', image)
+                cv2.namedWindow(self.name, cv2.WND_PROP_FULLSCREEN)
+                cv2.moveWindow(self.name, 1920, 0)
+                cv2.setWindowProperty(self.name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+                cv2.imshow(self.name, image)
                 cv2.waitKey(1)
                 await asyncio.sleep(0.01)
         else:
@@ -450,9 +451,10 @@ class Window(Tk):
                 if self.status == True:
                     break
                 ret, image = cam.read()
-                cv2.namedWindow(image)
-                cv2.moveWindow(image, 1024, 0)
-                cv2.imshow('Imagetest', image)
+                cv2.namedWindow(self.name, cv2.WND_PROP_FULLSCREEN)
+                cv2.moveWindow(self.name, 1920, 0)
+                cv2.setWindowProperty(self.name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+                cv2.imshow(self.name, image)
                 cv2.waitKey(1)
                 await asyncio.sleep(0.01)
 
