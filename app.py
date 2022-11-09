2323f872e11c285584f1b10060de837c798ec0f5
Vitalik <tarnavskij2002@gmail.com>
2022-11-09 04:06:15 +0200

    cameras

--- app.py
+++ app.py
@@ -186,17 +186,17 @@ class Window(Tk):
                                              lambda: self.loop.create_task(self.smoke(11, self.btnSmoke5)), 0)
 
         self.btnCam1 = self.btn_father.btn(self.img_father.cam, self.img_father.cam_active,
-                                           lambda: self.loop.create_task(self.camEnable(0, self.btnCam1)), 0)
+                                           lambda: self.loop.create_task(self.camEnable(-1, self.btnCam1)), 0)
         self.btnCam2 = self.btn_father.btn(self.img_father.cam, self.img_father.cam_active,
-                                           lambda: self.loop.create_task(self.camEnable(1, self.btnCam2)), 0)
+                                           lambda: self.loop.create_task(self.camEnable(0, self.btnCam2)), 0)
         self.btnCam3 = self.btn_father.btn(self.img_father.cam, self.img_father.cam_active,
-                                           lambda: self.loop.create_task(self.camEnable(2, self.btnCam3)), 0)
+                                           lambda: self.loop.create_task(self.camEnable(1, self.btnCam3)), 0)
         self.btnCam4 = self.btn_father.btn(self.img_father.cam, self.img_father.cam_active,
-                                           lambda: self.loop.create_task(self.camEnable(3, self.btnCam4)), 0)
+                                           lambda: self.loop.create_task(self.camEnable(2, self.btnCam4)), 0)
         self.btnCam5 = self.btn_father.btn(self.img_father.cam, self.img_father.cam_active,
-                                           lambda: self.loop.create_task(self.camEnable(4, self.btnCam5)), 0)
+                                           lambda: self.loop.create_task(self.camEnable(3, self.btnCam5)), 0)
         self.btnCam6 = self.btn_father.btn(self.img_father.cam, self.img_father.cam_active,
-                                           lambda: self.loop.create_task(self.camEnable(5, self.btnCam6)), 0)
+                                           lambda: self.loop.create_task(self.camEnable(4, self.btnCam6)), 0)
 
         self.btnFire1 = self.btn_father.btn(self.img_father.fire, self.img_father.fire_active,
                                             lambda: self.loop.create_task(self.fire(16, 31, self.btnFire1)), 0)
