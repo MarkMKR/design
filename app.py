436911bfe296f3ae85033cfd7756b8a44c0dc3e2
Vitalik <tarnavskij2002@gmail.com>
2022-11-10 15:52:56 +0200

    window size

--- app.py
+++ app.py
@@ -441,9 +441,6 @@ class Window(Tk):
                     if self.status == False:
                         break
                     ret, image = cam.read()
-                    cv2.namedWindow(self.name, cv2.WND_PROP_FULLSCREEN)
-                    cv2.moveWindow(self.name, 1920, 0)
-                    cv2.setWindowProperty(self.name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
                     cv2.imshow(self.name, image)
                     cv2.waitKey(1)
                     await asyncio.sleep(0.01)
@@ -452,9 +449,6 @@ class Window(Tk):
                     if self.status == True:
                         break
                     ret, image = cam.read()
-                    cv2.namedWindow(self.name, cv2.WND_PROP_FULLSCREEN)
-                    cv2.moveWindow(self.name, 1920, 0)
-                    cv2.setWindowProperty(self.name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
                     cv2.imshow(self.name, image)
                     cv2.waitKey(1)
                     await asyncio.sleep(0.01)
