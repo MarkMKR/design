ed18cc1b5744031934c8e5114faf0284201138d2
Vitalik <tarnavskij2002@gmail.com>
2022-12-18 20:12:06 +0200

    move windows

--- app.py
+++ app.py
@@ -38,6 +38,7 @@ def camPreview(previewName, camID, event):
     while rval:
         cv2.namedWindow(previewName, cv2.WND_PROP_FULLSCREEN)
         cv2.setWindowProperty(previewName, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+        cv2.moveWindow(previewName, 1024, 0)
         cv2.imshow(previewName, frame)
         rval, frame = cam.read()
         key = cv2.waitKey(20)
